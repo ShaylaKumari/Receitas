93db85fabab14b867d3ca5f3ec71d67850ae6af7
Ananda Shayla <anandashayla@hotmail.com>
2025-02-25 18:32:47 -0300

    Ajustes finais diários

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ botaoTopo.addEventListener('click', () => {
 function redirecionar(url) {
     window.location.href = url;
 }
+
+// Pesquisar
+document.addEventListener("DOMContentLoaded", function() {
+    const barraPesquisa = document.getElementById("barra-pesquisa");
+    const receitasContainer = document.getElementById("naveg-receitas");
+    const receitas = receitasContainer.getElementsByClassName("campo-padrao");
+
+    barraPesquisa.addEventListener("input", function() {
+        const pesquisa = barraPesquisa.value.toLowerCase();
+
+        Array.from(receitas).forEach(function(receita) {
+            const figcaption = receita.getElementsByTagName("figcaption")[0].textContent.toLowerCase();
+
+            // Verifica se a sequência digitada está presente no figcaption
+            if (figcaption.includes(pesquisa)) {
+                receita.style.display = ""; // Exibe o item se corresponder
+            } else {
+                receita.style.display = "none"; // Oculta o item se não corresponder
+            }
+        });
+    });
+});
